0c3cec4674797b1b88a22b28175d5b73a829d225
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2023-01-11 19:43:03 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+void maxMin();
+int main(){
+    maxMin();
+}
+void maxMin() {
+    int max, min, value, num;
+    int i = 10;
+    std::cin >> value;
+    num = value % 10;
+    max = num;
+    min = num;
+    for (; i <= value; i *= 10) {
+        if (max < value % i / (i / 10)) max = value % i / (i / 10);
+        if (min > value % i / (i / 10)) min = value % i / (i / 10);
+    }
+    if (max < value / (i / 10)) max = value / (i / 10);
+    if (min > value / (i / 10)) min = value / (i / 10);
+    std::cout << "Max: " << max << '\n' << "Min: " << min;
+}
+
